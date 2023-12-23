82aead76747dc557b9f69f7716ffbe46047d9545
Christian Guevara <chrisdb150@gmail.com>
2023-12-23 00:12:48 -0400

    solving types error when import @coffe-collection/components in the app

--- apps/coffe-collection-frontend/app/page.tsx
+++ apps/coffe-collection-frontend/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
 
-// eslint-disable-next-line no-use-before-define
-import { Header } from "@coffe-collection/components"
+import { Header } from '@coffe-collection/components'
 
 export default function Home() {
   return (
     <div className="App">
-      <Header text="Hello World from coffe collection" />
+      <Header  text="Hello World from coffe collection" />
     </div>
   )
 }
--- packages/components/src/components/Header.tsx
+++ packages/components/src/components/Header.tsx
@@ -3,5 +3,5 @@ export interface HeaderText {
     text: string
 }
 export const Header = ({ text }: HeaderText) => {
-    return <div className="text">{ text }</div>
+    return <div>{ text }</div>
 }
